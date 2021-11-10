767b994f52a679cf1b1bfe0c2f2b46e771e4c30e
Alex Olson <alexkellenolson@gmail.com>
2021-11-10 22:53:04 +0100

    remove demo and target

--- build-demo.sh
+++ build-demo.sh
@@ -8,5 +8,6 @@ git checkout gh-pages
 git checkout main -- demo
 cp demo/* .
 git reset HEAD demo
+rm -rf target/ demo/
 git commit -am 'update demo'
 git push && git checkout main || git checkout main
