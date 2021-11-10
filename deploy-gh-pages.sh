b75e37ee72220d1c86a7054c32b8fc525028dd67
Alex Olson <alexkellenolson@gmail.com>
2021-11-10 23:11:11 +0100

    no need to checkout since directory is untracked

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -2,10 +2,8 @@
 
 # deploy demo to gh-pages
 git checkout gh-pages
-git checkout main -- demo
 cp demo/* .
 git reset HEAD demo
-rm -rf target/ demo/
 git commit -am 'update demo'
 git push
 git checkout main
