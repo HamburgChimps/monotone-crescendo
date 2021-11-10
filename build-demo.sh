cc35580786a8327088d4d2586994879bb2bf81c5
Alex Olson <alexkellenolson@gmail.com>
2021-11-10 22:38:24 +0100

    add build script and demo dir

---
+++ build-demo.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash -xe
+
+# build for gh-pages
+cargo build --target wasm32-unknown-unknown --release
+wasm-gc target/wasm32-unknown-unknown/release/monotone_crescendo.wasm
+cp target/wasm32-unknown-unknown/release/monotone_crescendo.wasm demo/
+git checkout gh-pages
+git checkout main -- demo
+cp demo/* .
+git commit -am 'update demo'
+git push
+git checkout main
