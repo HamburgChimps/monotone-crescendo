f0ae9857712135bbd8622d88e922966f85e5f440
Alex Olson <alexkellenolson@gmail.com>
2021-11-10 23:02:21 +0100

    make demo dir if doesnt exist

--- build-demo.sh
+++ build-demo.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash -x
 
 # build demo directory
+mkdir -p demo
 cargo build --target wasm32-unknown-unknown --release
 wasm-gc target/wasm32-unknown-unknown/release/monotone_crescendo.wasm
 cp target/wasm32-unknown-unknown/release/monotone_crescendo.wasm demo/
