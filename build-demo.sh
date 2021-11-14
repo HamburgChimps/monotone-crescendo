0d194eaec4d59628d3c4de22a91ad6bf6b284003
Alex Olson <alexkellenolson@gmail.com>
2021-11-15 00:20:44 +0100

    [wip] fix build demo script

--- build-demo.sh
+++ build-demo.sh
@@ -4,7 +4,7 @@
 mkdir demo
 cargo build --target wasm32-unknown-unknown --release
 wasm-gc target/wasm32-unknown-unknown/release/monotone_crescendo.wasm
-cago doc
+cargo doc
 cp target/wasm32-unknown-unknown/release/monotone_crescendo.wasm demo/
-cp target/doc demo
+cp -r target/doc demo
 cp demo.html demo/index.html
