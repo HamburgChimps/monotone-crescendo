7a30b166983bc9006794590b9995d9bdb1ddf114
Alex Olson <alexkellenolson@gmail.com>
2021-11-16 02:22:12 +0100

    close #2.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//! A library for calculating the least number of flips needed to make a binary string monotone increasing.
+//! Also contains utility functions for allocating webassembly memory and returning a pointer to it, as well
+//! as deallocating said memory. It also contains a function that can be called via WebAssembly, and thus Javascript.
 use std::{cmp::min, ffi::CStr, ffi::CString, i32::MAX, mem, os::raw::c_void};
 
 #[cfg(test)]
@@ -52,19 +55,29 @@ pub unsafe extern "C" fn make_me_monotone_crescendo(ptr: *mut u8) {
 /// I find the explanation on LeetCode to this solution somewhat overcomplicated and hard to understand, so I will attempt
 /// to explain it (hopefully) a bit simpler here.
 ///
-/// The first thing this solution does is calculate the [prefix sum][2] of every item in the string and store it in an array where the index
-/// of the array corresponds to the index of the character in the string. So for example, if we had the string `11010`, the prefix sum array
-/// for that string would look like: `[0,1,2,2,3,3]`
-/// 
-/// Left off here...
+/// The first thing this solution does is calculate the [prefix sum][2] of every item in the string and store it in an array. So for example, if we had the string `11010`, the prefix sum array
+/// for that string would look like: `[0,1,2,2,3,3]`. This prefix sum array represents the the number of 1's that are present before the corresponding character index in the string.
 ///
-/// This solution works by looping through the given string and, for each character in the string, analyzing the string as two halves partioned
+/// This solution then works by looping through the given string and, for each character in the string, analyzing the string as two halves partioned
 /// at that character. The number of 1's in the first half are added to the number of 0's in the second half to determine the total number of flips
 /// needed to make the string monotone increasing for that location in the string.
+///
+/// We get the number of 1's in the first half by looking up the corresponding index in the prefix sum array.
+/// We get the number of 0's in the second half by subtracting the number of 1's in the second half from the length
+/// of the second half.
 /// 
+/// We then compare this number to the current minimum flips value and take the minimum of these two values again. The minimium
+/// we have in the end is what we return.
+/// 
+/// Note: the prefix sum array could also be optimized to remove the redundant element at index 0. The way the solution on LeetCode
+/// calculates the prefix sum array, the first element will always be zero. For example, if we had the string `11010`, the prefix sum array
+/// could be optimized to `[1,2,2,3,3]`. Apparently the official solution calculates the prefix sum array with the redundant zero at the beginning
+/// to [prevent some sort of possible overflow condition][3], out of principle more than anything else.
+///
 /// [0]: <https://leetcode.com/problems/flip-string-to-monotone-increasing/solution/>
 /// [1]: <https://leetcode.com/problems/flip-string-to-monotone-increasing/>
 /// [2]: <https://en.wikipedia.org/wiki/Prefix_sum>
+/// [3]: <https://leetcode.com/problems/flip-string-to-monotone-increasing/solution/1047706>
 pub fn monotone_crescendo_prefix_sums(s: &str) -> i32 {
     let str_size = s.chars().count() as i32;
     let mut prefix_sums = vec![0; str_size as usize + 1];
