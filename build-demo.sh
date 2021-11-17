0b3be010e172de3b6831cbefdecf549901a868c4
Alex Olson <alexkellenolson@gmail.com>
2021-11-17 02:59:05 +0100

    use wasm-gc after all even though it says not to as it gets the compiled wasm binary the smallest out of all the things I tried.

--- build-demo.sh
+++ build-demo.sh
@@ -3,6 +3,7 @@
 # build demo directory
 mkdir -p demo
 cargo build --target wasm32-unknown-unknown --release
+wasm-gc target/wasm32-unknown-unknown/release/monotone_crescendo.wasm
 cargo doc
 cp target/wasm32-unknown-unknown/release/monotone_crescendo.wasm demo/
 cp -r target/doc demo
