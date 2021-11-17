be5b563bedcad08d6b50c01e892e1cfc052400e8
Alex Olson <alexkellenolson@gmail.com>
2021-11-17 02:33:32 +0100

    no need to run wasm-gc, rust compiler now supports --gc-options which apparently does same/similar thing (cut cruft off of compiled wasm)

--- build-demo.sh
+++ build-demo.sh
@@ -2,8 +2,7 @@
 
 # build demo directory
 mkdir -p demo
-cargo build --target wasm32-unknown-unknown --release
-wasm-gc target/wasm32-unknown-unknown/release/monotone_crescendo.wasm
+cargo build --target wasm32-unknown-unknown -- release
 cargo doc
 cp target/wasm32-unknown-unknown/release/monotone_crescendo.wasm demo/
 cp -r target/doc demo
