2311f3b662e4ce73b517ba07f37679a5bde7e119
Alex Olson <alexkellenolson@gmail.com>
2021-11-17 04:06:52 +0100

    update demo

---
+++ doc/monotone_crescendo/ffi/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["alloc","Allocate memory in WebAssembly’s linear memory and return its offset"],["dealloc","Deallocate memory in WebAssembly’s linear memory at the offset located at `ptr`"],["make_me_monotone_crescendo",""]]});
